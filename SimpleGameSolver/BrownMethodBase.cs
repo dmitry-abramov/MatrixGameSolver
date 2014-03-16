899ab231215d0d347801a2ef2379f6cceb66fdd0
dmitry abramov <abramov.dmitry92@gmail.com>
2014-03-16 08:55:22 +0000

    -added two realization of Brown method for SimpleGameSolver

---
+++ SimpleGameSolver/BrownMethodBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleGameSolver
+{
+    public abstract class BrownMethodBase
+    {
+        public int IterationsCount { get; private set; }
+
+        public BrownMethodBase(int iterationCount)
+        {
+            IterationsCount = iterationCount;
+        }
+
+        public abstract List<List<List<int>>> Solve(double[,] fpp, double[,] spp, List<int> fpsp, List<int> spsp);
+    }
+}
