11bfdf51b51eb331bb4d7ea76e2814b65d399f8b
dmitry abramov <abramov.dmitry92@gmail.com>
2014-11-10 18:45:12 +0000

    added BaricentricConverter2D

---
+++ GameTheoryUtils/BaricentricConverter2D.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace GameTheoryUtils
+{
+    public class BaricentricConverter2D
+    {
+        private const double epsilon = 0.000001;
+
+        private PointF firstVertex;
+        private PointF secondVertex;
+        private PointF thirdVertex;
+
+        public PointF FirstVertex 
+        {
+            get
+            {
+                return firstVertex;
+            }
+            set 
+            {
+                if (IsOnSameLine(value, SecondVertex, ThirdVertex))
+                {
+                    throw new ArgumentException("Basis points should not be os the same line");
+                }
+
+                firstVertex = value;
+            }
+        }
+
+        public PointF SecondVertex
+        {
+            get
+            {
+                return secondVertex;
+            }
+            set
+            {
+                if (IsOnSameLine(FirstVertex, value, ThirdVertex))
+                {
+                    throw new ArgumentException("Basis points should not be os the same line");
+                }
+
+                secondVertex = value;
+            }
+        }
+
+        public PointF ThirdVertex
+        {
+            get
+            {
+                return thirdVertex;
+            }
+            set
+            {
+                if (IsOnSameLine(FirstVertex, SecondVertex, value))
+                {
+                    throw new ArgumentException("Basis points should not be os the same line");
+                }
+
+                thirdVertex = value;
+            }
+        }
+
+        public BaricentricConverter2D()
+            : this(new PointF(0, 0), new PointF(0, 1), new PointF(1, 0))
+        { 
+        }
+
+        public BaricentricConverter2D(PointF first, PointF second, PointF third)
+        {
+            if (IsOnSameLine(first, second, third))
+            {
+                throw new ArgumentException("Basis points should not be os the same line");
+            }
+
+            FirstVertex = first;
+            SecondVertex = second;
+            ThirdVertex = third;
+        }
+
+        private bool IsOnSameLine(PointF first, PointF second, PointF third)
+        {
+            var k1 = (second.Y - first.Y) / (second.X - first.X);
+            var k2 = (third.Y - first.Y) / (third.X - first.X);
+
+            if (double.IsNaN(k1) && double.IsNaN(k2))
+            {
+                return true;
+            }
+
+            return Math.Abs(k1 - k2) < epsilon;
+        }
+    }
+}
---
+++ UnitTests/BaricentricConverter2DTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameTheoryUtils;
+using System.Drawing;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BaricentricConverter2DTest
+    {
+        [TestMethod]
+        public void DefaultBasis()
+        {
+            var converter = new BaricentricConverter2D();
+                        
+            Assert.AreEqual(new PointF(0, 0), converter.FirstVertex);
+            Assert.AreEqual(new PointF(0, 1), converter.SecondVertex);
+            Assert.AreEqual(new PointF(1, 0), converter.ThirdVertex);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void BasisPointsFromSameLine()
+        {
+            new BaricentricConverter2D(new PointF(0, 0), new PointF((float)0.5, 1), new PointF(1, 2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetWrongBasisOnVerticalLine()
+        {
+            var converter = new BaricentricConverter2D();
+
+            converter.ThirdVertex = new PointF(0, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetWrongBasis()
+        {
+            var converter = new BaricentricConverter2D();
+
+            converter.SecondVertex = new PointF(10, 0);
+        }
+    }
+}
