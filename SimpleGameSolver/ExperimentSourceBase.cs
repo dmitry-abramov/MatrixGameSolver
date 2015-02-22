c285dbed6bc82349411a2379f231a35c5ec52f3a
dmitry abramov <abramov.dmitry92@gmail.com>
2015-02-22 13:00:27 +0000

    chnage IExperimentSource to ExperimentSourceBase

---
+++ SimpleGameSolver/ExperimentSourceBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleGameSolver
+{
+    public abstract class ExperimentSourceBase
+    {
+        public abstract string Name { get; }
+
+        public abstract string Description { get; }
+
+        public abstract IList<Experiment> GetExperiments();
+    }
+}
--- SimpleGameSolver/IEnumerableExtensions.cs
+++ SimpleGameSolver/IEnumerableExtensions.cs
@@ -7,7 +7,7 @@ namespace SimpleGameSolver
 {
     public static class IEnumerableExtensions
     {
-        public static IEnumerable<double> Normalize<ulong>(this IEnumerable<ulong> collection)
+        public static IEnumerable<double> Normalize(this IEnumerable<ulong> collection)
         {
             var sum = collection.Sum(u => (decimal)u);
             return collection.Select(u => (double)(u / sum));
