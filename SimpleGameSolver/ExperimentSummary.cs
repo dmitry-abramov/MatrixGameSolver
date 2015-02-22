9c1cd15059675f39dec892697f75ae4684e67adc
dmitry abramov <abramov.dmitry92@gmail.com>
2015-02-22 16:46:42 +0000

    added Experimentator simple realization

---
+++ SimpleGameSolver/ExperimentSummary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleGameSolver
+{
+    public class ExperimentSummary
+    {
+        public Experiment Experiment { get; private set; }
+
+        public Situation Result { get; private set; }
+
+        public ExperimentSummary(Experiment experiment, Situation result)
+        {
+            Experiment = experiment;
+            Result = result;
+        }
+    }
+}
--- SimpleGameSolver/Experimentator.cs
+++ SimpleGameSolver/Experimentator.cs
@@ -9,13 +9,31 @@ namespace SimpleGameSolver
     {
         public ExperimentSourceBase ExperimentSource { get; private set; }
 
-        public Experimentator(ExperimentSourceBase experimentSource)
+        public BrownMethodBase SolveMethod { get; private set; }
+
+        public double Progress { get; private set; }
+
+        public Experimentator(ExperimentSourceBase experimentSource, BrownMethodBase solveMethod)
         {
+            Progress = 0;
             ExperimentSource = experimentSource;
+            SolveMethod = solveMethod;
         }
 
         public void MakeExperiment()
         {
+            var summarys = new List<ExperimentSummary>();
+            var experiments = ExperimentSource.GetExperiments();
+
+            for (int i = 0; i < experiments.Count(); i++)
+            {
+                Progress = i / experiments.Count();
+
+                var experiment = experiments[i];
+                var experimentResult = SolveMethod.Solve(experiment.Game, experiment.StartSituation, experiment.Parameters);
+
+                summarys.Add(new ExperimentSummary(experiment, experimentResult.Result));
+            }
         }
     }
 }
